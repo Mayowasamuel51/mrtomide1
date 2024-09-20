c977332acf9ff4341d37ebc2ca75db87e4690ed3
mayowasamuel <fpasamuelmayowa51@gmail.com>
2024-09-20 16:44:45 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        'primary': '#842281',
+      }
+    },
+  },
+  plugins: [],
+}
+
